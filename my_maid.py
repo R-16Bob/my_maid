fe0f82896b7ca0da8d266bcf7aa12f7dad98e4e4
BobCheng <42957089+R-16Bob@users.noreply.github.com>
2020-09-05 23:58:21 +0800

    first_update
    增加了便宜菜单；
    增加了打开网易云音乐的功能；
    修改了WEEK()函数，使它能全年计算出当前教学周；
    增加了一个问候语，毕竟，她是为了爱我而被创造的。

--- my_maid.py
+++ my_maid.py
@@ -2,12 +2,20 @@
 import time
 import random
 import sys
+import os
 
 #  提供可爱的不同时间的问候
 def lovely_maid(): 
     print ("现在是:",localtime.tm_year,'年',localtime.tm_mon,'月',localtime.tm_mday,'日',localtime.tm_hour,'时',localtime.tm_min,'分')
-    week()
-    print('='*30,'\n主人，欢迎回来！(｡･∀･)ﾉﾞ')
+    # ============开学后把vication改成FALSE
+    vication = True
+    if(vication):
+        print('现在是假期中，主人要好好生活哦')
+    else:
+        week()
+    print('='*55,'主人，欢迎回来！(｡･∀･)ﾉﾞ')
+    print("我是您的女仆爱丽丝，无论过去、现在还是未来，都爱着您哦\n(≧ω≦)/ ")
+    print('='*55)
     hour = localtime.tm_hour
     if(0<=hour<=4 or 23<=hour<=24):
         print('主人，夜深了，您还不睡吗？\n请您今天早点休息吧。要我陪您睡也是可以的哦(*/ω＼*)')
@@ -19,28 +27,76 @@ def lovely_maid():
     elif(15<=hour<=19):
         print('主人，下午的时间用来学习也不错呢\n学累了就睡我膝上吧(/ω＼*)(脸红)')
     else:
-        print('主人辛苦一天了，现在可以休息一下哦o(^▽^)o')
+        print('主人辛苦一天了，现在可以放松一下哦o(^▽^)o')
 
 #  使用算法告诉计算现在是第几周
 def week():
+
+    # 先判断闰年与否, rn为真表示是闰年
+    year = localtime.tm_year
+    if(year % 4 == 0):
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                rn =True
+            rn = False
+        else:
+            rn = True
+    else:
+        rn = False
     #  如何判定当前周数？
-    d = {2:29,3:31,4:30,5:31,6:30,7:31}
+    d1 = {1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31} # 闰年 
+    d2 = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31} # 非闰年
+    if(rn):
+        d = d1
+    else:
+        d = d2
     mon  = localtime.tm_mon
     day = localtime.tm_mday
     # mon = int(input('mon:')) 测试
     # day = int(input('day:'))
     i = mon
-    num = 0  # 存储与2.24之差
-    while(i>=2):
-        if(mon==2):
-            num += day-24
-        elif(i==mon):
-            num += day
-        else:
-            num += d[i]
-        i -=1
+    # =================================
 
-    print('现在是大二第二学期的第',int(num/7+1),'周哦')
+    # 要在此处写好开学日期
+    # begin_mon = 9  # 开学月份,并没有用，请改下面的if
+    begin_day = 13  # 开学日期
+    # =================================
+    num = 0  # 存储与开学日期之差
+    
+    if(mon>=2 and mon<=6):  # 默认2月开学
+        while(i>=2): 
+            if(i==2):
+                if(mon == 2):
+                    num += day - begin_day
+                else:
+                    num += d[2] - begin_day
+            elif(i==mon):
+                num += day
+            else:
+                num += d[i]
+            i -= 1       
+    elif(mon == 1 or (mon>=9 and mon<=12)):   # 默认9月开学
+        while(i>=9 or i == 1): 
+            if(i==9):
+                if(mon == 9):
+                    num += day - begin_day
+                else:
+                    num += d[9] - begin_day
+            elif(i==mon):
+                num += day
+            else:
+                num += d[i]
+            if(i==1):
+                i=12
+            else:
+                i -= 1        
+    else: # 剩下7-8月，i取-1，表示假期中，也可以强行设置i=-1
+        i=-1
+
+    if(i == -1):
+        print('现在是假期中，主人要好好生活哦')
+    else:
+        print('现在是大三第一学期的第',int(num/7+1),'周哦')  # 很遗憾，大几第几学期要每学期重写一下
 
 #  伪人工智能的聊天功能,可以随意调教
 def chat():
@@ -51,7 +107,7 @@ def chat():
         if(str == '我也爱你'):
             print('ヾ(≧▽≦*)o')
         elif(str == '再见' or str =='bye'):
-            print('主人不要人家了吗 o(TヘTo)\n人家会一直等你哦')
+            print('o(TヘTo)\n只要主人需要，小爱一直都在哦')
             input()
             sys.exit()
         elif(str == '谢谢'):
@@ -88,11 +144,16 @@ def lovey_chat():
         print('\n主人，要再来一个吗？')
         s = input('>')
 
-#  非常常见的功能菜单
-def menu():
+def music():
+    print('已为您打开网易云音乐，音乐是一种药呢。')
+    os.system("cd C:\\Program Files (x86)\\Netease\\CloudMusic && cloudmusic.exe")
+
+#  非常常见的功能菜单,实际是聊天
+def ask():
     while 1:
-        s = input('主人，要来聊天吗？~\(≧▽≦)/~(兴奋)\n>')
-        if(s=='好啊'or s=='好' or s=='来聊天吧' or s=='y' or s=='yes'):
+        input()
+        s = input('主人，要来聊天吗？~\\(≧▽≦)/~(兴奋)\n>')
+        if(s=='好啊'or s=='好' or s=='嗯' or s=='y' or s=='yes'):
             chat()
         elif(s=='bye' or s=='再见'):
             print('主人我会想你的o(TヘTo)')
@@ -106,7 +167,19 @@ def menu():
             print('主人，开心点了吗?( •̀ ω •́ )✧')
             input('>')
             print('您开心就好了呢')
-        
+
+def menu():
+    while(1):
+        print('='*55,'需要爱丽丝为您做什么吗？')
+        command=input(prompt)
+        if(command =='c'): # 聊天
+            ask()
+        if(command=='e'): # 退出
+            break 
+        if(command == 'cm'): ## 打开网易云音乐
+            music()
+			
+prompt = ">>"          
     
 
     
@@ -115,3 +188,4 @@ while 1:
     lovely_maid()
     menu()
     input()
+
