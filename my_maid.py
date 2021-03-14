88f69759e94cc10170de26bd969478dcf9dba3a5
BobCheng <42957089+R-16Bob@users.noreply.github.com>
2021-03-14 17:15:00 +0800

    update countDown()
    增加考研倒计时功能

--- my_maid.py
+++ my_maid.py
@@ -7,8 +7,9 @@ import os
 #  提供可爱的不同时间的问候
 def lovely_maid(): 
     print ("现在是:",localtime.tm_year,'年',localtime.tm_mon,'月',localtime.tm_mday,'日',localtime.tm_hour,'时',localtime.tm_min,'分')
+    countDown()
     # ============开学后把vication改成FALSE
-    vication = True
+    vication = False
     if(vication):
         print('现在是假期中，主人要好好生活哦')
     else:
@@ -34,6 +35,9 @@ def week():
 
     # 先判断闰年与否, rn为真表示是闰年
     year = localtime.tm_year
+    # rn = False
+    # if((year % 4==0 and not year % 100 == 0) or year % 400 == 0):
+    #     rn = True
     if(year % 4 == 0):
         if(year % 100 == 0):
             if(year % 400 == 0):
@@ -43,6 +47,7 @@ def week():
             rn = True
     else:
         rn = False
+    
     #  如何判定当前周数？
     d1 = {1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31} # 闰年 
     d2 = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31} # 非闰年
@@ -58,18 +63,18 @@ def week():
     # =================================
 
     # 要在此处写好开学日期
-    # begin_mon = 9  # 开学月份,并没有用，请改下面的if
-    begin_day = 13  # 开学日期
+    begin_mon = 3  # 开学月份
+    begin_day = 1  # 开学日期
     # =================================
     num = 0  # 存储与开学日期之差
     
-    if(mon>=2 and mon<=6):  # 默认2月开学
-        while(i>=2): 
-            if(i==2):
-                if(mon == 2):
+    if(mon>=begin_mon and mon<=6):  # 默认2月开学
+        while(i>=begin_mon): 
+            if(i==begin_mon):
+                if(mon == begin_mon):
                     num += day - begin_day
                 else:
-                    num += d[2] - begin_day
+                    num += d[begin_mon] - begin_day
             elif(i==mon):
                 num += day
             else:
@@ -96,7 +101,7 @@ def week():
     if(i == -1):
         print('现在是假期中，主人要好好生活哦')
     else:
-        print('现在是大三第一学期的第',int(num/7+1),'周哦')  # 很遗憾，大几第几学期要每学期重写一下
+        print('现在是大三第二学期的第',int(num/7+1),'周哦')  # 很遗憾，大几第几学期要每学期重写一下
 
 #  伪人工智能的聊天功能,可以随意调教
 def chat():
@@ -115,6 +120,8 @@ def chat():
         elif(str=='不聊了'):
             print('好哒，主人')
             break
+        elif(str=='l'):
+            lovely_maid()
         else:
             print(str)
 
@@ -184,7 +191,28 @@ def menu():
             break
 			
 prompt = ">>"          
-    
+
+def countDown():
+    # 获取今天的日期
+    month = m = time.localtime().tm_mon
+    day = time.localtime().tm_mday
+    # 计算今天到12月25日的差值
+    d2 = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    count = 0
+    if month < 12:
+        while m < 12:
+            if m == month:  # 如果是当月,增加差值
+                count += d2[month] - day
+            else:
+                count += d2[m]  # 不是当月,则增加当月天数
+            m += 1
+            if m == 12:
+                count += 25
+    elif month == 12:
+        count += d2[12] - day
+    else:
+        count = 0
+    print("距离22年研究生考试初试还有{}天".format(count))
 
     
 while 1:
